59996e27e1b1fe9e9e354f0405565a18140c9c3b
Alireza Jalili <alirlikh@gmail.com>
2025-01-27 09:35:34 +0330

    feat: footer

---
+++ app/_component/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer className="conatiner mx-auto mt-20">
+      <div className="w-full h-64 text-center">
+        <span className="text-sm">Fun Game</span>
+      </div>
+    </footer>
+  );
+}
+export default Footer;
