3a4cef5b65efe4ea6dce7bafebf245b6b1847cdf
Alireza Jalili <alirlikh@gmail.com>
2025-01-27 09:58:56 +0330

    feat: add header

---
+++ app/_component/Header.tsx
@@ -0,0 +1,10 @@
+import Logo from "./Logo";
+
+function Header() {
+  return (
+    <header className="conatiner mx-auto place-items-center">
+      <Logo />
+    </header>
+  );
+}
+export default Header;
