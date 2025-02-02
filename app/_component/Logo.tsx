3737c34396c234544d1bfb047d2f55637b1a1590
Alireza Jalili <alirlikh@gmail.com>
2025-02-02 13:16:19 +0330

    fix: logo comonent

--- app/_component/Logo.tsx
+++ app/_component/Logo.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import logo from "@/public/images/logo.png";
+import logo from "@/public/images/logo2.svg";
 
 function Logo() {
   return (
-    <div className=" relative w-full h-56">
-      <Image className="object-cover" fill src={logo} alt="logo image" />
+    <div className=" relative w-full h-56 bg-blackBlue ">
+      <Image className="object-contain py-6" fill src={logo} alt="logo image" />
     </div>
   );
 }
