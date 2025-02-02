2d4ff36c88ea47a24530563fc6745abe9524f80f
Alireza Jalili <alirlikh@gmail.com>
2025-02-02 11:40:15 +0330

    fix:image selection

---
+++ app/_component/GameBoard.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import GameCards from "./GameCards";
+import {
+  addFlippedCard,
+  checkMatchedCardWithDelay,
+  enableTimer,
+} from "../_lib/features/gameSlice/gameSlice";
+import { useAppDispatch, useAppSelector } from "../hooks/hook";
+import Spiner from "./Spiner";
+
+function GameBoard() {
+  const dispatch = useAppDispatch();
+  const { cards, timeStarter, loading, flippedCards, isChecking } =
+    useAppSelector((state) => state.gameState);
+
+  // function handleClick(id: number) {
+  //   if (!timeStarter) {
+  //     dispatch(enableTimer());
+  //   }
+  //   dispatch(addFlippedCard(id));
+  //   // setTimeout(() => {
+  //   //   dispatch(checkMatchedCard());
+  //   // }, 1000);
+  // }
+
+  function handleClick(id: number) {
+    if (!timeStarter) {
+      dispatch(enableTimer());
+    }
+    if (!isChecking) {
+      dispatch(addFlippedCard(id));
+      if (flippedCards.length === 1) {
+        dispatch(checkMatchedCardWithDelay());
+      }
+    }
+  }
+
+  return (
+    <div className="p-4 border-4 border-black text-center rounded-lg ">
+      <div className="w-[448px] min-h-96 flex flex-row gap-2 flex-wrap justify-around items-center ">
+        {loading ? (
+          <Spiner />
+        ) : (
+          cards?.map((item, index: number) => {
+            return (
+              <GameCards
+                onClick={() => handleClick(item.id)}
+                image={item}
+                index={index}
+                key={item.id}
+              />
+            );
+          })
+        )}
+      </div>
+    </div>
+  );
+}
+export default GameBoard;
