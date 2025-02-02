6b4b50369ca43c1c5571c0fc6329189ea7397f82
Alireza Jalili <alirlikh@gmail.com>
2025-02-02 16:53:37 +0330

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Header from "./_component/Header";
 import Footer from "./_component/Footer";
 import GameBoard from "./_component/GameBoard";
 import GameHeader from "./_component/GameHeader";
+import GameAreaFooter from "./_component/GameAreaFooter";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <section className="grid grid-cols-1  place-items-center">
           <GameHeader />
           <GameBoard />
+          <GameAreaFooter />
         </section>
       </main>
       <Footer />
