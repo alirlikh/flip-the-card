54a3c96160e9e5dfaa3e2aa81d28ca5de099e107
Alireza Jalili <alirlikh@gmail.com>
2025-02-02 10:00:33 +0330

    feat: add spinner

---
+++ app/_component/Spiner.tsx
@@ -0,0 +1,8 @@
+function Spiner() {
+  return (
+    <div>
+      <div className="border-4 border-r-4 border-r-black border-transparent spin-in-180 animate-spin repeat-infinite  w-10 h-10 rounded-[50%]"></div>
+    </div>
+  );
+}
+export default Spiner;
