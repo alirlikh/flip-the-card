c54f2408290b1c701c286fb0fd9d7ccffb6879f9
Alireza Jalili <alirlikh@gmail.com>
2025-02-02 11:55:11 +0330

    feat: game home design

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
-import ImageCardPoster from "@/app/_component/ImageCardPoster";
-import ImageCard from "./_component/ImageCard";
+import Header from "./_component/Header";
+import Footer from "./_component/Footer";
+import GameBoard from "./_component/GameBoard";
+import GameHeader from "./_component/GameHeader";
 
 export default function Home() {
   return (
-    <div>
-      <ImageCardPoster cardNumber={1} />
-      <ImageCard />
-    </div>
+    <>
+      <Header />
+      <main className="container mx-auto place-items-center">
+        <section className="grid grid-cols-1  place-items-center">
+          <GameHeader />
+          <GameBoard />
+        </section>
+      </main>
+      <Footer />
+    </>
   );
 }
