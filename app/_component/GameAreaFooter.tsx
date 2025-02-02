73f189101e7fdff0216e33b742c493fd33244e7b
Alireza Jalili <alirlikh@gmail.com>
2025-02-02 16:53:16 +0330

    fix: change restart button to the end of game table

---
+++ app/_component/GameAreaFooter.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/hooks/use-toast";
+import { useAppDispatch, useAppSelector } from "../hooks/hook";
+import { resetGame } from "../_lib/features/gameSlice/gameSlice";
+
+function GameAreaFooter() {
+  const dispatch = useAppDispatch();
+  const { timeStarter } = useAppSelector((state) => state.gameState);
+
+  const { toast } = useToast();
+  const handleReset = () => {
+    dispatch(resetGame());
+    toast({
+      title: "Game Restarted",
+      description: "You can try Again!",
+      variant: "destructive",
+    });
+  };
+  return (
+    <>
+      {!!timeStarter && (
+        <div className="p-5 mt-2">
+          <Button onClick={handleReset} variant={"destructive"} size={"lg"}>
+            Restart
+          </Button>
+        </div>
+      )}
+    </>
+  );
+}
+export default GameAreaFooter;
