969fa64c2c337a0e3d573e5a58c102ba95485aa6
Alireza Jalili <alirlikh@gmail.com>
2025-02-02 17:31:35 +0330

    fix: footer

--- app/_component/Footer.tsx
+++ app/_component/Footer.tsx
@@ -1,8 +1,9 @@
 function Footer() {
   return (
-    <footer className="conatiner mx-auto mt-20">
-      <div className="w-full h-64 text-center">
-        <span className="text-sm">Fun Game</span>
+    <footer className="conatiner mx-auto mt-20 bg-blackBlue text-white pt-2 ">
+      <div className="w-full h-10 text-center leading-[1px]">
+        <span className="text-base font-bold block"> Game</span>
+        <span className="text-xs  inline-block">just for fun</span>
       </div>
     </footer>
   );
