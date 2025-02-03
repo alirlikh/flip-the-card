f8faaaaebc49ef19724db54a205a25e7ebc46c8e
Alireza Jalili <alirlikh@gmail.com>
2025-02-03 16:32:31 +0330

    fix: game status checker

--- app/_component/GameHeader.tsx
+++ app/_component/GameHeader.tsx
@@ -5,9 +5,10 @@ import {
   initializeCard,
   setGameStatus,
   setLoading,
+  setScore,
   timeCounter,
 } from "../_lib/features/gameSlice/gameSlice";
-import { useCallback, useEffect, useMemo } from "react";
+import { useCallback, useEffect } from "react";
 import CategorySelect from "./CategorySelect";
 import { GameResultAlert } from "./GameResultAlert";
 import { ScoreHistory } from "./ScoreHistory";
@@ -22,32 +23,31 @@ function GameHeader() {
     category,
     timeStarter,
     isWon,
-    matchedCards,
-    flippedCards,
+    score,
   } = useAppSelector((state) => state.gameState);
   const remainMove = maxMoves - moves > 0 ? maxMoves - moves : 0;
 
-  const score = useMemo(() => {
-    if (isWon) {
-      return matchedCards.length * 10 + timer * 2;
-    } else {
-      const calculatedScore =
-        matchedCards.length * 10 - (moves - matchedCards.length) * 2;
-      return calculatedScore > 0 ? calculatedScore : 0;
-    }
-  }, [matchedCards, timer, moves, isWon]);
+  // const score = useMemo(() => {
+  //   if (isWon) {
+  //     return matchedCards.length * 10 + timer * 2;
+  //   } else {
+  //     const calculatedScore =
+  //       matchedCards.length * 10 - (moves - matchedCards.length) * 2;
+  //     return calculatedScore > 0 ? calculatedScore : 0;
+  //   }
+  // }, [matchedCards, timer, moves, isWon]);
 
-  const gameSatus = useCallback(() => {
-    if (timer <= 0) {
-      dispatch(setGameStatus());
-    } else if (remainMove <= 0) {
-      dispatch(setGameStatus());
-    }
-  }, [timer, remainMove, dispatch]);
+  const gameStatus = useCallback(() => {
+    dispatch(setGameStatus());
+  }, [dispatch]);
 
   useEffect(() => {
-    gameSatus();
-  }, [flippedCards, matchedCards, timer]);
+    gameStatus();
+  }, [timer, remainMove, isWon, gameStatus, gameOver]);
+
+  useEffect(() => {
+    dispatch(setScore());
+  }, [gameOver, isWon, dispatch]);
 
   useEffect(() => {
     dispatch(setLoading(true));
@@ -83,7 +83,7 @@ function GameHeader() {
         {(gameOver || isWon) && (
           <GameResultAlert
             open={gameOver || isWon}
-            result={gameOver ? "lose" : "win"}
+            result={!isWon ? "lose" : "win"}
           />
         )}
 
