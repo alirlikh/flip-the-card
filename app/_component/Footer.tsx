5b4c506e1872d93c693f2c94eca06ab59287437c
Alireza Jalili <alirlikh@gmail.com>
2025-02-04 17:31:35 +0330

    feat: add author link in footer

--- app/_component/Footer.tsx
+++ app/_component/Footer.tsx
@@ -1,9 +1,14 @@
 function Footer() {
   return (
-    <footer className="conatiner mx-auto mt-20 bg-blackBlue text-white pt-2 ">
-      <div className="w-full h-10 text-center leading-[1px]">
+    <footer className="conatiner mx-auto mt-20 bg-blackBlue text-white pt-3 pb-2 ">
+      <div className="w-full  text-center leading-[1px]">
         <span className="text-base font-bold block"> Game</span>
-        <span className="text-xs  inline-block">just for fun</span>
+        <span className="text-xs block">just for fun</span>
+        <span className="text-xs  inline-block underline">
+          <a href={process.env.NEXT_PUBLIC_AUTHOR_LINK}>
+            Developed By {process.env.NEXT_PUBLIC_AUTHOR}
+          </a>
+        </span>
       </div>
     </footer>
   );
