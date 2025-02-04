0cb3c6843938ce50e48b7b67f85dfba996405fb8
Alireza Jalili <alirlikh@gmail.com>
2025-02-04 10:36:06 +0330

    fix: image priority

--- app/_component/Logo.tsx
+++ app/_component/Logo.tsx
@@ -4,7 +4,14 @@ import logo from "@/public/images/logo2.svg";
 function Logo() {
   return (
     <div className=" relative w-full h-56 bg-blackBlue ">
-      <Image className="object-contain py-6" fill src={logo} alt="logo image" />
+      <Image
+        className="object-contain py-6"
+        fill
+        src={logo}
+        priority
+        alt="logo image"
+        quality={80}
+      />
     </div>
   );
 }
