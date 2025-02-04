1ce34ce7b2d10096c9f25ef7a0904b54bd1203eb
Alireza Jalili <alirlikh@gmail.com>
2025-02-04 10:23:15 +0330

    feat: add scroll to score history

--- app/_component/ScoreHistory.tsx
+++ app/_component/ScoreHistory.tsx
@@ -7,12 +7,13 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import ScoreTooltip from "./ScoreTooltip";
 import { Score } from "../_utils/types";
 import { useEffect, useMemo, useState } from "react";
 import { getScoreFromStorage } from "../_utils/RandWLocalStorage";
 import { useAppSelector } from "../hooks/hook";
-import { Calendar, Pin, PinIcon } from "lucide-react";
+import { Calendar, PinIcon } from "lucide-react";
 
 export function ScoreHistory() {
   const { gameOver, isWon } = useAppSelector((state) => state.gameState);
@@ -55,47 +56,48 @@ export function ScoreHistory() {
             List of Score By Date That You Have Played
           </SheetDescription>
         </SheetHeader>
+        <ScrollArea className="h-full pb-16">
+          {savedScore && savedScore ? (
+            Object.entries(scoresByDate).map(([date, scores]) => (
+              <div key={date} className="space-y-2">
+                <div className="text-gray-400 text-sm mt-3 flex flex-row gap-x-2 items-center">
+                  <Calendar className="w-4" />
 
-        {savedScore && savedScore ? (
-          Object.entries(scoresByDate).map(([date, scores]) => (
-            <div key={date} className="space-y-2">
-              <div className="text-gray-400 text-sm mt-3 flex flex-row gap-x-2 items-center">
-                <Calendar className="w-4" />
-
-                <time>{new Date(date).toLocaleDateString()}</time>
-              </div>
-              <ul>
-                {scores.map((score, index: number) => (
-                  <ScoreTooltip
-                    score={score.score}
-                    result={score.result}
-                    key={index}
-                  >
-                    <li>
-                      <div
-                        className={`flex flex-row items-center justify-between px-3 font-semibold text-lg ${score.result === "win" ? "text-green" : "text-red-800"}`}
-                      >
-                        <div className="text-center">
-                          <span>Score:</span>
-                        </div>
+                  <time>{new Date(date).toLocaleDateString()}</time>
+                </div>
+                <ul>
+                  {scores.map((score, index: number) => (
+                    <ScoreTooltip
+                      score={score.score}
+                      result={score.result}
+                      key={index}
+                    >
+                      <li>
                         <div
-                          className={`flex-1 border-b border-dashed ${score.result === "win" ? "border-b-green" : "border-b-red-800"}`}
-                        ></div>
-                        <div className="text-center">
-                          <span>{score.score}</span>
+                          className={`flex flex-row items-center justify-between px-3 font-semibold text-lg ${score.result === "win" ? "text-green" : "text-red-800"}`}
+                        >
+                          <div className="text-center">
+                            <span>Score:</span>
+                          </div>
+                          <div
+                            className={`flex-1 border-b border-dashed ${score.result === "win" ? "border-b-green" : "border-b-red-800"}`}
+                          ></div>
+                          <div className="text-center">
+                            <span>{score.score}</span>
+                          </div>
                         </div>
-                      </div>
-                    </li>
-                  </ScoreTooltip>
-                ))}
-              </ul>
+                      </li>
+                    </ScoreTooltip>
+                  ))}
+                </ul>
+              </div>
+            ))
+          ) : (
+            <div className="text-red-800 mt-20 mx-auto px-5">
+              <p>You don&apos;t have Score Data!</p>
             </div>
-          ))
-        ) : (
-          <div className="text-red-800 mt-20 mx-auto px-5">
-            <p>You don&apos;t have Score Data!</p>
-          </div>
-        )}
+          )}
+        </ScrollArea>
       </SheetContent>
     </Sheet>
   );
